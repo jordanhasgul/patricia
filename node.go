a0d88413e6d843c842fbf1feb071e75eee7b9277
Jordan Hasgul <jxh1243@student.bham.ac.uk>
2023-06-06 21:25:08 +0100

    implemented helper function to obtain the nth bit from a byte slice

--- node.go
+++ node.go
@@ -1,5 +1,7 @@
 package patricia
 
+import "math/bits"
+
 type node[T any] struct {
 	key []byte
 	fdb int
@@ -20,4 +22,13 @@ func (n *node[T]) put(key []byte, value T) bool {
 
 func (n *node[T]) remove(key []byte) bool {
 	return false
+}
+
+func nthBit(buf []byte, n int) byte {
+	if n < 0 || n >= 8*len(buf) {
+		return 0
+	}
+
+	msb := (buf[n/8] << (n % 8)) & 128
+	return bits.RotateLeft8(msb, 1)
 }
