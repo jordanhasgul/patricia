449e1b26fa8ef4829cf6c6ae70bf3bb439cf7597
Jordan Hasgul <jxh1243@student.bham.ac.uk>
2023-06-06 21:26:01 +0100

    implemented helper function to find the first differing bit between two byte slices

--- node.go
+++ node.go
@@ -32,3 +32,35 @@ func nthBit(buf []byte, n int) byte {
 	msb := (buf[n/8] << (n % 8)) & 128
 	return bits.RotateLeft8(msb, 1)
 }
+
+func firstDifferingBit(buf1, buf2 []byte) int {
+	if len(buf1) == 0 && len(buf2) != 0 {
+		return bits.LeadingZeros8(buf2[0])
+	}
+
+	if len(buf1) != 0 && len(buf2) == 0 {
+		return bits.LeadingZeros8(buf1[0])
+	}
+
+	n := len(buf1)
+	if len(buf2) > n {
+		n = len(buf2)
+	}
+
+	newBuf1 := make([]byte, n)
+	copy(newBuf1, buf1)
+
+	newBuf2 := make([]byte, n)
+	copy(newBuf2, buf2)
+
+	var differingByte int
+	for idx := 0; idx < n; idx++ {
+		if newBuf1[idx] != newBuf2[idx] {
+			differingByte = idx
+			break
+		}
+	}
+
+	differingBit := newBuf1[differingByte] ^ newBuf2[differingByte]
+	return 8*differingByte + bits.LeadingZeros8(differingBit)
+}
