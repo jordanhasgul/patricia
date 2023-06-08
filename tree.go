17e42e1b1a8e6b9b74c4dc42b55e2a97bcda4aff
Jordan Hasgul <jxh1243@student.bham.ac.uk>
2023-06-08 02:30:23 +0100

    added documentation to exported API

--- tree.go
+++ tree.go
@@ -2,8 +2,10 @@ package patricia
 
 import "bytes"
 
-type VisitFunc[T any] func([]byte, T) bool
-
+// Tree represents a generic Patricia tree i.e. a radix tree
+// with radix r = 2. It is an associative data structure that
+// maintains a collection of key-value pairs of type []byte
+// and type T, respectively.
 type Tree[T any] struct {
 	root    *node[T]
 	rootSet bool
@@ -11,6 +13,7 @@ type Tree[T any] struct {
 	size int
 }
 
+// New returns a pointer to a zero-valued [patricia.Tree].
 func New[T any]() *Tree[T] {
 	root := &node[T]{
 		key: []byte{},
@@ -29,6 +32,10 @@ func New[T any]() *Tree[T] {
 	}
 }
 
+// Get returns the value asssociated with the key and a boolean
+// indicating whether the key was present within the tree.
+//
+// Note: calls to Get are idempotent.
 func (t *Tree[T]) Get(key []byte) (T, bool) {
 	if bytes.Equal(t.root.key, key) {
 		return t.root.value, t.rootSet
@@ -37,6 +44,11 @@ func (t *Tree[T]) Get(key []byte) (T, bool) {
 	return t.root.get(key)
 }
 
+// Put creates an association between the key and the value. If
+// the key was present within the tree, its associated value is
+// overwritten.
+//
+// Note: calls to Put are idempotent.
 func (t *Tree[T]) Put(key []byte, value T) {
 	if bytes.Equal(t.root.key, key) {
 		t.root.value = value
@@ -52,6 +64,9 @@ func (t *Tree[T]) Put(key []byte, value T) {
 	}
 }
 
+// Remove deletes any association involving the key and a value.
+//
+// Note: calls to Remove are idempotent.
 func (t *Tree[T]) Remove(key []byte) {
 	if bytes.Equal(t.root.key, key) {
 		t.root.value = *new(T)
@@ -67,6 +82,13 @@ func (t *Tree[T]) Remove(key []byte) {
 	}
 }
 
+type VisitFunc[T any] func([]byte, T) bool
+
+// Visit traverses the tree in-order and applies f to any key-value
+// pairs whose key begins with the prefix.
+//
+// The traversal terminates once f has returned true. Otherwise, it
+// terminates once every key-value pair has been visited.
 func (t *Tree[T]) Visit(prefix []byte, f VisitFunc[T]) {
 	if t.rootSet && bytes.HasPrefix(t.root.key, prefix) {
 		if f(t.root.key, t.root.value) {
@@ -77,6 +99,7 @@ func (t *Tree[T]) Visit(prefix []byte, f VisitFunc[T]) {
 	t.root.visit(prefix, f)
 }
 
+// Size returns the number of associations within the tree.
 func (t *Tree[T]) Size() int {
 	return t.size
 }
